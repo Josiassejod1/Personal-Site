6b0e6cafc3d25815adaf00db1ec147ab1d6148a0
Josiassejod1 <dalvin.josias@gmail.com>
2016-09-23 23:49:57 -0400

    Website Essentials

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    
+    setBindings();
+    
+    
+    
+    
+    
+});
+
+function setBindings() {
+    $("nav a").click(function(e){
+        e.preventDefault();
+        var sectionID = e.currentTarget.id + "Section";
+       // alert("button id " + e.currentTarget.id);
+       
+       $("html body").animate({
+            
+            scrollTop: $("#" + sectionID).offset().top
+        
+        }, 1000)
+        
+        
+        
+        })
+    
+}
+        
+   
